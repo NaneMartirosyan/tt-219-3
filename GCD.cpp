4f17d84e78634ca6e1e1346c465f0888833587ee
NaneMartirosyan <nanemartirosyan.tt219-2@polytechnic.am>
2024-09-11 14:15:29 +0400

    Create GCD.cpp

---
+++ GCD.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int baj(int n, int m){
+    if(n==0)
+         return m;
+    return baj(n%m,m); 
+}
+int main() {
+    int N,M;
+    std::cout<<"Nermuteq N ev M tvery"<<std::endl;
+    std::cin>>N>>M;
+    std::cout<<baj(abs(N),abs(M))<<std::endl;
+    
+    
+    
+    
+
+    return 0;
+}
