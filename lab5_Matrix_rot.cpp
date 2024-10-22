96121fc6347827162c594b3d9663d853790d110e
NaneMartirosyan <nanemartirosyan.tt219-2@polytechnic.am>
2024-10-22 22:40:49 +0400

    Update lab5_Matrix.cpp

--- lab5_Matrix_rot.cpp
+++ lab5_Matrix_rot.cpp
@@ -78,3 +78,88 @@ int main() {
 
     return 0;
 }
+
+// #include <iostream>
+// #include <string>
+// #include <vector>
+// #include <iomanip>
+
+// class FixedMatrixEncryptor {
+// private:
+//     char matrix[5][3]; 
+//     const int rows = 5;
+//     const int cols = 3;
+
+//     void clearMatrix() {
+//         for (int i = 0; i < rows; ++i)
+//             for (int j = 0; j < cols; ++j)
+//                 matrix[i][j] = ' ';
+//     }
+
+//     void fillMatrix(const std::string& sentence) {
+//         clearMatrix();
+//         int row = 0, col = 0;
+
+//         for (char ch : sentence) {
+//             if (ch == ' ') { 
+//                 col++; 
+//                 if (col >= cols) { 
+//                     col = 0;
+//                     row++;
+//                 }
+//                 continue;
+//             }
+            
+//             matrix[row][col++] = ch;
+            
+//             if (col >= cols) { 
+//                 col = 0;
+//                 row++;
+//             }
+//             if (row >= rows) break; 
+//         }
+//     }
+
+// public:
+//     FixedMatrixEncryptor(const std::string& sentence) {
+//         fillMatrix(sentence);
+//     }
+
+//     std::string encrypt() {
+//         std::string encryptedText;
+
+        
+//         for (int col = 0; col < cols; ++col) {
+//             for (int row = 0; row < rows; ++row) {
+//                 if (matrix[row][col] != ' ') {
+//                     encryptedText += matrix[row][col];
+//                 }
+//             }
+//             encryptedText += ' '; 
+//         }
+
+//         return encryptedText;
+//     }
+
+//     void displayMatrix() {
+//         for (int i = 0; i < rows; ++i) {
+//             for (int j = 0; j < cols; ++j) {
+//                 std::cout << std::setw(2) << matrix[i][j];
+//             }
+//             std::cout << std::endl;
+//         }
+//     }
+// };
+
+// int main() {
+//     std::string sentence = "A big cat 21";
+//     FixedMatrixEncryptor encryptor(sentence);
+
+//     std::cout << "Matrix layout:" << std::endl;
+//     encryptor.displayMatrix();
+
+//     std::cout << "\nEncrypted text: " << encryptor.encrypt() << std::endl;
+
+//     return 0;
+// }
+
