6bb2383c7cf93660769a87c0d626f76ea5480489
NaneMartirosyan <nanemartirosyan.tt219-2@polytechnic.am>
2024-10-29 21:27:05 +0400

    Update GCD.cpp

--- GCD.cpp
+++ GCD.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int baj(int n, int m){
+    n=abs(n);
+    m=abs(m);
     if(n==0)
          return m;
     return baj(m%n,n); 
@@ -9,6 +11,6 @@ int main() {
     int N,M;
     std::cout<<"Nermuteq N ev M tvery"<<std::endl;
     std::cin>>N>>M;
-    std::cout<<baj(abs(N),abs(M))<<std::endl;
+    std::cout<<baj(N,M)<<std::endl;
     return 0;
 }
