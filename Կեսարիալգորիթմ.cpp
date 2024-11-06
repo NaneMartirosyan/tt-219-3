56db7158858e495510086c31739190021042247c
NaneMartirosyan <nanemartirosyan.tt219-2@polytechnic.am>
2024-11-06 10:43:06 +0400

    Update Կեսարիալգորիթմ.cpp

--- Կեսարիալգորիթմ.cpp
+++ Կեսարիալգորիթմ.cpp
@@ -6,14 +6,20 @@ private:
     int shift;
 
 public:
+   
     CaesarCipher(int s) {
         shift = s;
     }
-
     std::string encrypt(const std::string& plaintext) {
         std::string result = "";
         for (char ch : plaintext) {
-            result += static_cast<char>((static_cast<unsigned char>(ch) + shift) % 256);
+            if (isupper(ch)) {
+                result += char(int(ch + shift) );
+            } else if (islower(ch)) {
+                result += char(int(ch + shift));
+            } else {
+                result += ch;
+            }
         }
         return result;
     }
@@ -21,30 +27,36 @@ public:
     std::string decrypt(const std::string& ciphertext) {
         std::string result = "";
         for (char ch : ciphertext) {
-            result += static_cast<char>((static_cast<unsigned char>(ch) - shift + 256) % 256);
+            if (isupper(ch)) {
+                result += char(int(ch - shift) );
+            } else if (islower(ch)) {
+                result += char(int(ch - shift));
+            } else {
+                result += ch;
+            }
         }
         return result;
     }
 };
 
-int main() {
-    int shift;
-    std::cout << "Enter shift value: ";
-    std::cin >> shift;
+// int main() {
+//     int shift;
+//     std::cout << "Enter shift value: ";
+//     std::cin >> shift;
 
-    std::cin.ignore();
+//     std::cin.ignore();
 
-    CaesarCipher cipher(shift);
+//     CaesarCipher cipher(shift);
 
-    std::string message;
-    std::cout << "Enter the message to encrypt: ";
-    std::getline(std::cin, message);
+//     std::string message;
+//     std::cout << "Enter the message to encrypt: ";
+//     std::getline(std::cin, message);
 
-    std::string encrypted = cipher.encrypt(message);
-    std::cout << "Encrypted: " << encrypted << std::endl;
+//     std::string encrypted = cipher.encrypt(message);
+//     std::cout << "Encrypted: " << encrypted << std::endl;
 
-    std::string decrypted = cipher.decrypt(encrypted);
-    std::cout << "Decrypted: " << decrypted << std::endl;
+//     std::string decrypted = cipher.decrypt(encrypted);
+//     std::cout << "Decrypted: " << decrypted << std::endl;
 
-    return 0;
-}
+//     return 0;
+// }
